784aec221fb0a2a555a3b90d235461de6017a36d
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-09-20 09:23:12 -0300

    Conhecendo as props

--- src/components/shared/imagemCinza/index.js
+++ src/components/shared/imagemCinza/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ImagemCinza = () => {
+const ImagemCinza = (props) => {
 	return(
 		<div>
-			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg" alt="Planeta mercúrio"/> 
+			<img src={props.img_url} alt={ props.alt_valor }></img> 
 		</div>
 	)
 }
