d3bfb77283303283d3cb9f94780f1b86b3125905
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-09-21 09:52:37 -0300

    Acrescentando o hook Link para redirecionar a screens de dado planeta - sem reload

--- src/components/planetas/planeta/index.js
+++ src/components/planetas/planeta/index.js
@@ -1,16 +1,20 @@
 import React, {Fragment} from "react";
 import ImagemCinza from '../../shared/imagemCinza';
 import DescricaoComLink from '../../shared/descricaoComLink';
+import { Link } from 'react-router-dom';
+
 
 const Planeta = (props) => {
 
 return(
 	<Fragment>		
-		<h4>{props.nome}</h4>
+		<Link to={`/planeta/${props.id}`}>
+			<h4>{props.nome}</h4>
+		</Link>
 		<DescricaoComLink link={props.link} descricao={props.descricao} nome={props.nome}/>
 		<ImagemCinza img_url={ props.img_url } alt_valor={ props.alt_valor } />
 	</Fragment>
 	)		
 }
 
-export default Planeta
+export default Planeta;
